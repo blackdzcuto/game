e56a0de3399d56915477368b990f0046cab29e11
WaifuCat <131111416+WaifuCat@users.noreply.github.com>
2023-08-30 07:20:25 +0700

    Update mining.js

--- Game/mining.js
+++ Game/mining.js
@@ -13,12 +13,12 @@ const config = {
 
 const langData = {
   'vi_VN': {
-    'noaccount': 'Bạn chưa có tài khoản',
-    'haveaccount': 'Bạn đã có tài khoản',
-    'success': 'Thành công',
-    'error': 'Lỗi',
+    'noaccount': '[⚜️] ➜ Bạn chưa có tài khoản',
+    'haveaccount': '[⚜️] ➜ Bạn đã có tài khoản',
+    'success': '[⚜️] ➜ Thành công',
+    'error': '[⚜️] ➜ Lỗi',
     'menu': 'Chưa có',
-    'info': 'Thông tin người chơi\nCấp độ: {level}\nKinh nghiệm: {exp}\n\nThông tin khu vực\n{mapInfo}\n\nThông tin trang bị\n{equipInfo}'
+    'info': '[⚜️]Thông tin người chơi[⚜️]\n[⚜️] ➜ Cấp độ: {level}\n[⚜️] ➜ Kinh nghiệm: {exp}\n\n[⚜️]Thông tin khu vực[⚜️]\n{mapInfo}\n\n[⚜️]Thông tin trang bị[⚜️]\n{equipInfo}'
   }
 };
 
@@ -49,6 +49,7 @@ async function loadData() {
 
 async function onCall({ message, args, getLang }) {
   const targetID = message.senderID;
+  const { Users } = global.controllers;
 
   if (args.length === 0) {
     message.reply(getLang("menu"));
@@ -61,13 +62,13 @@ async function onCall({ message, args, getLang }) {
     } else {
       const equippedItem = items[user.equip];
       const equippedItemInfo = equippedItem
-        ? `Tên: ${equippedItem.name}\nTốc độ: ${equippedItem.speed}\nThời gian chờ: ${equippedItem.countdown}`
-        : 'Không có thông tin';
+        ? `[⚜️] ➜ Tên: ${equippedItem.name}\n[⚜️] ➜ Tốc độ: ${equippedItem.speed}\n[⚜️] ➜ Thời gian chờ: ${equippedItem.countdown}`
+        : '[⚜️] ➜ Không có thông tin';
 
       const userMapData = maps[user.map];
       const userMapInfo = userMapData
-        ? `Khu vực: ${userMapData.name}\nBổ trợ: x${userMapData.buff}`
-        : 'Không có thông tin';
+        ? `[⚜️] ➜ Khu vực: ${userMapData.name}\n[⚜️] ➜ Bổ trợ: x${userMapData.buff}`
+        : '[⚜️] ➜ Không có thông tin';
 
       const infoMessage = getLang("info")
         .replace('{level}', user.lv)
@@ -110,7 +111,7 @@ async function onCall({ message, args, getLang }) {
 
     const equippedItem = items[user.equip];
     if (!equippedItem) {
-      message.reply("Bạn chưa có trang bị để đào.");
+      message.reply("[⚜️] ➜ Bạn chưa có trang bị để đào.");
       return;
     }
 
@@ -120,18 +121,20 @@ async function onCall({ message, args, getLang }) {
 
     if (currentTime - lastMineTime < cooldownDuration) {
       const remainingCooldown = Math.ceil((lastMineTime + cooldownDuration - currentTime) / 1000);
-      message.reply(`Bạn cần đợi ${remainingCooldown} giây để tiếp tục đào.`);
+      message.reply(`[⚜️] ➜ Bạn cần đợi ${remainingCooldown} giây để tiếp tục đào.`);
       return;
     }
 
     const userMapData = maps[user.map];
     if (!userMapData) {
-      message.reply("Không có thông tin về khu vực.");
+      message.reply("[⚜️] ➜ Không có thông tin về khu vực.");
       return;
     }
 
     const minedAmount = equippedItem.speed;
     const expEarned = minedAmount * userMapData.buff;
+
+    await Users.increaseMoney(targetID, expEarned);
 
     cooldowns[targetID] = currentTime;
     try {
@@ -143,11 +146,93 @@ async function onCall({ message, args, getLang }) {
       }
 
       await fs.writeFile('plugins/commands/Game/data.json', JSON.stringify(data, null, 2));
-      message.reply(`Bạn đã đào được ${minedAmount} đơn vị tài nguyên và nhận ${expEarned} exp. Tổng exp của bạn là ${user.exp} và cấp độ là ${user.lv}.`);
+      message.reply(`[⚜️] ➜ Bạn đã đào được ${minedAmount} quặng và nhận được ${expEarned} kinh nghiệm,${expEarned} $. Tổng Kinh nghiệm của bạn là ${user.exp} và cấp độ hiện tại là ${user.lv}.`);
     } catch (error) {
       console.error(error);
       message.reply(getLang("error"));
     }
+  } else if (args[0] === 'upgrade') {
+    const user = data[targetID];
+    if (!user) {
+      message.reply(getLang("noaccount"));
+      return;
+    }
+
+    const selectedItem = items[user.equip];
+    if (!selectedItem) {
+      message.reply("[⚜️] ➜ Bạn chưa có trang bị để nâng cấp.");
+      return;
+    }
+
+    const upgradeLevel = Number(args[1]);
+    const targetItem = items[`item${upgradeLevel}`]; 
+
+    if (!targetItem) {
+      message.reply("[⚜️] ➜ Không tìm thấy vật phẩm để nâng cấp.");
+      return;
+    }
+
+    const upgradeCost = targetItem.price;
+
+    const userMoney = await Users.getMoney(targetID);
+    if (userMoney < upgradeCost) {
+      message.reply("[⚜️] ➜ Bạn không có đủ tiền để nâng cấp vật phẩm.");
+      return;
+    }
+
+    const currentTime = Date.now();
+    const lastUpgradeTime = cooldowns[targetID] || 0;
+    const upgradeCooldown = 60 * 60 * 1000;
+
+    if (currentTime - lastUpgradeTime < upgradeCooldown) {
+      const remainingCooldown = Math.ceil((lastUpgradeTime + upgradeCooldown - currentTime) / 1000);
+      message.reply(`[⚜️] ➜ Bạn cần đợi ${remainingCooldown} giây để tiếp tục nâng cấp.`);
+      return;
+    }
+
+    try {
+      await Users.decreaseMoney(targetID, upgradeCost);
+
+      user.equip = `item${upgradeLevel}`;
+
+      await fs.writeFile('plugins/commands/Game/data.json', JSON.stringify(data, null, 2));
+      message.reply(`[⚜️] ➜ Bạn đã mua ${targetItem.name} với giá ${upgradeCost} $`);
+      cooldowns[targetID] = currentTime;
+    } catch (error) {
+      console.error(error);
+      message.reply(getLang("error"));
+    }
+  } else if (args[0] === 'teleport') {
+    const user = data[targetID];
+    if (!user) {
+      message.reply(getLang("noaccount"));
+      return;
+    }
+
+    const targetMapLevel = Number(args[1]);
+    const userMapData = maps[user.map];
+
+    if (!userMapData) {
+      message.reply("[⚜️] ➜ Không có thông tin về khu vực hiện tại.");
+      return;
+    }
+
+    const targetMapData = maps[`map${targetMapLevel}`];
+
+    if (!targetMapData) {
+      message.reply("[⚜️] ➜ Không tìm thấy thông tin về map bạn muốn dịch chuyển đến.");
+      return;
+    }
+
+    if (user.lv < targetMapData.lv) {
+      message.reply("[⚜️] ➜ Bạn không đủ cấp độ để dịch chuyển đến map này.");
+      return;
+    }
+
+    user.map = `map${targetMapLevel}`;
+
+    await fs.writeFile('plugins/commands/Game/data.json', JSON.stringify(data, null, 2));
+    message.reply(`[⚜️] ➜ Bạn đã được dịch chuyển đến map ${targetMapData.name}.`);
   }
 }
 
